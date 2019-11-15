93f370d9c7f3ebfef2a84fc83c41b759191ff7a0
infinite-Zh <zhxuwork@163.com>
2019-11-15 13:21:16 +0800

    烟花效果

---
+++ app/src/main/java/com/infinite/explode/particle/FireworkParticle.kt
@@ -0,0 +1,44 @@
+package com.infinite.explode.particle
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+
+/**
+ * @author bug小能手
+ * Created on 2019/11/15.
+ */
+class FireworkParticle(color:Int,cx:Int,cy:Int) :AbsParticle(color,cx, cy){
+    private var vx = 0.toDouble()
+    private var vy = (-150).toDouble()
+
+    companion object {
+        const val A_Y = 9.8
+    }
+
+    init {
+        vx = (Math.random() - 0.5f).times(20)
+    }
+
+    private val paint = Paint().apply {
+        this.color = color
+    }
+
+    override fun move(canvas: Canvas) {
+        paint.color = color
+        canvas.drawRect(
+            cx.toFloat(), cy.toFloat(),
+            cx + 12.toFloat(), cy + 12.toFloat(), paint
+        )
+    }
+
+    override fun updatePosition(factor: Float) {
+        cx += vx.toInt()
+        cy += vy.toInt()
+        vy += A_Y
+    }
+
+    override fun outOfZone(rect: Rect): Boolean {
+        return !rect.contains(cx,cy)
+    }
+}
