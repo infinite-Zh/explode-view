8278793d6abffd6fad60e2dc494db29eb7028f3d
infinite-Zh <zhxuwork@163.com>
2019-11-15 10:14:44 +0800

    添加重力下落粒子工厂

---
+++ app/src/main/java/com/infinite/explode/particle/GravityParticleFactory.kt
@@ -0,0 +1,34 @@
+package com.infinite.explode.particle
+
+import android.graphics.Bitmap
+
+/**
+ * @author bug小能手
+ * Created on 2019/11/15.
+ */
+class GravityParticleFactory : IParticleFactory {
+    override fun createParticles(bitmap: Bitmap, count: Int): MutableList<AbsParticle> {
+        val particles = mutableListOf<AbsParticle>()
+        var particle: AbsParticle
+        var sampleX = 1
+        var sampleY = 1
+        val w = bitmap.width
+        val h = bitmap.height
+
+        while (w.times(h).div(sampleX.times(sampleY)) > count) {
+            sampleX++
+            sampleY++
+        }
+
+
+        for (i in 0 until bitmap.width step sampleX) {
+            for (j in 0 until bitmap.height step sampleY) {
+                particle = GravityParticle(
+                    bitmap.getPixel(i, j), i, j
+                )
+                particles.add(particle)
+            }
+        }
+        return particles
+    }
+}
